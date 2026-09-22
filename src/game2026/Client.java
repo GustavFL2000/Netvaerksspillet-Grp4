a0b70a86fdc3741b7ef2a9c4c87ffc80f598547a
RedNickster <n.stenlokke@gmail.com>
2026-09-22 09:11:28 +0200

    Point besked lavet, ikke implementeret

--- src/game2026/Client.java
+++ src/game2026/Client.java
@@ -9,7 +9,7 @@ public class Client {
     DataOutputStream write;
 
     public Client() throws IOException {
-        Socket clientSocket = new Socket("localhost", 9999);
+        Socket clientSocket = new Socket("10.10.139.60", 9999);
         System.out.println("Forbindelse forbundet");
 
         write = new DataOutputStream(clientSocket.getOutputStream());
@@ -19,6 +19,10 @@ public class Client {
 
     public void movedMessage(int x, int y, String direction) throws IOException {
         write.writeBytes(String.format("MOVE %d %d %s%n", x, y, direction));
+    }
+    
+    public void pointMessage(String name, int points) throws IOException {
+        write.writeBytes(String.format("POINTS %s %d", name, points));
     }
 
     public static void main(String[] args) throws Exception {
